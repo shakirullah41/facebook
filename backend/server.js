7faf90c421e4361507172f848dd27258360c94c7
shakirullah41 <shakirullah41@users.noreply.github.com>
2022-07-20 10:35:46 +0500

    setup env

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
 const app = express();
 const { readdirSync } = require('fs');
 
+dotenv.config();
+app.use(cors());
 readdirSync('./routes').map((route) => {
   app.use(`/${route.split('.')[0]}`, require(`./routes/${route}`));
 });
-app.listen(4000, () => {
-  console.log('app is running at port 4000');
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`app is running at port ${PORT}`);
 });
