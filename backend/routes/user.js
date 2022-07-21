d1ff9a81efb56ad2a5cec8848d695292fc441958
shakirullah41 <shakirullah41@users.noreply.github.com>
2022-07-21 16:33:16 +0500

    mongodb setup and connection

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,7 @@
 const express = require('express');
+const controller = require('../controllers/user');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('this is user api');
-});
+router.get('/', controller.getUsers);
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = express();
 const { readdirSync } = require('fs');
 
 dotenv.config();
+
+app.use(express.json());
 app.use(cors());
 readdirSync('./routes').map((route) => {
   app.use(`/${route.split('.')[0]}`, require(`./routes/${route}`));
 });
+mongoose
+  .connect(process.env.DATABASE_URL)
+  .then(() => console.log('Database connect successfully'))
+  .catch((err) => console.log(err));
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`app is running at port ${PORT}`);
