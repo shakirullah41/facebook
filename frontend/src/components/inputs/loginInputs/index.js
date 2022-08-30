9bd6accf66f1422b944f3097fcc441acf81a83a0
shakirullah41 <shakirullah41@users.noreply.github.com>
2022-08-30 17:18:54 +0500

    desktop with error messages

--- frontend/src/components/inputs/loginInputs/index.js
+++ frontend/src/components/inputs/loginInputs/index.js
@@ -1,8 +1,11 @@
 import './style.css';
 import { ErrorMessage, useField } from 'formik';
+import { useMediaQuery } from 'react-responsive';
 export default function LoginInput({ placeholder, type, bottom, ...props }) {
   const [field, meta] = useField(props);
-  console.log(field);
+  const desktopView = useMediaQuery({
+    query: '(min-width: 950px)',
+  });
   return (
     <div className='input_wrap'>
       {meta.touched && meta.error && !bottom && (
@@ -20,11 +23,16 @@ export default function LoginInput({ placeholder, type, bottom, ...props }) {
         {...props}
       />
       {meta && meta.touched && meta.error && (
-        <i className='error_icon' style={!bottom ? { top: '67%' } : {}}></i>
+        <i
+          className='error_icon'
+          style={!bottom && !desktopView ? { top: '67%' } : {}}
+        ></i>
       )}
       {meta.touched && meta.error && bottom && (
         <div className='input_error'>
-          <div className='error_arrow_bottom'></div>
+          <div
+            className={desktopView ? 'error_arrow_top' : 'error_arrow_bottom'}
+          ></div>
           <ErrorMessage name={field.name} />
         </div>
       )}
